f38f8c24970afbba54ad976a50a9bf40cb8daba1
Youthan Irigoyen <a01750476@itesm.mx>
2021-09-06 17:44:17 -0500

    Minor updates

--- ZFunctionAlgorithm/main.cpp
+++ ZFunctionAlgorithm/main.cpp
@@ -4,23 +4,22 @@
 using namespace std;
 
 /* Get Z array given a string combining the pattern and the original string separated by a special character */
-void getZ(string &s) {
-    // Define left and right bounds for z-boxes as well as the matching index
-    int l = 0, r = 0, m = 0;
-    // Start lookup from second element in z-array
-    for (int i = 1; i < s.size(); i++) {
-        // If there is no match currently
+void findPattern(const string &p, const string &s) {
+    // Create new string
+    string newStr = p + "$" + s;
+    // Define current z-box range to 0
+    size_t l = 0, r = 0;
+    // Set initial matching characters count
+    int count = 0;
+    // Set current lookup index
+    size_t index = 1;
+    // Start lookup (z-naive approach)
+    for (int i = 1; i < newStr.size(); i++) {
+        // If there is no particular match
         if (i > r) {
             
         }
     }
-}
-
-/* Find a pattern within a given string using the Z algorithm */
-void findPattern(const string &s, const string &p) {
-    // Create new string
-    string newStr = p + "$" + s;
-    // Find pattern within string
 } 
 
 int main() {
