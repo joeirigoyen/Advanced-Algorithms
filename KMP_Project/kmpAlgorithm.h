064140fad5ebd67e4e611c53136a72892c2b5cc8
Youthan Irigoyen <a01750476@itesm.mx>
2021-10-13 19:30:55 -0500

    Fixed minor issues

--- KMP_Project/kmpAlgorithm.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-
-#pragma once
-
-using namespace std;
-
-void findPrefix(string &pattern, int &m, vector<int> &prefArray);
-void kmpPattSearch(string &mainString, string &pattern, vector<int> &locArray, size_t &loc);
